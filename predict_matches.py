c0049b731dfa316d6343fb2b0f7be33ada29a703
Aryan Kumar Sinha <123369753+patermars@users.noreply.github.com>
2025-10-04 23:37:42 +0530

    updating

--- predict_matches.py
+++ predict_matches.py
@@ -9,14 +9,6 @@ predictor.predict_match(
     team1='FNATIC',
     team2='NRG',
     team1_winrate=0.8,
-    team2_winrate=0.8,
-    match_format='Bo3'
+    team2_winrate=1,
+    match_format='Bo5'
 )
-
-# predictor.predict_match(
-#     team1='G2 Esports',
-#     team2='Sentinels',
-#     team1_winrate=0.75,
-#     team2_winrate=0.68,
-#     match_format='Bo5'
-# )
